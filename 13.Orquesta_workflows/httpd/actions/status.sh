0702c14dd4b7eab1720d5161e0786b7dffe890af
NarendraPAutomationEngineer <narendra4st2@gmail.com>
2020-07-25 16:10:47 +0000

    updated httpd version wf

---
+++ 13.Orquesta_workflows/httpd/actions/status.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+OSID=$(cat /etc/os-release | grep -w "ID" | cut -d "=" -f 2)
+OSID=${OSID%\"}
+OSID=${OSID#\"}
+if [[ "${OSID}" == "ubuntu" ]]
+then
+  echo "The os is identified as Ubuntu"
+  systemctl status apache2
+elif [[ "${OSID}" == "rhel" ]]
+then
+  echo "The os is identified as RHEL"
+  systemctl status httpd
+else
+  echo "The os is unkown"
+  exit 5
+fi
+
