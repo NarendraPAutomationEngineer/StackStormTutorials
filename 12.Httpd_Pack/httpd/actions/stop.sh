21e7e99b4dedc2da9f81a6e03f6e3f597bc4bc4b
NarendraPAutomationEngineer <narendra4st2@gmail.com>
2020-08-08 23:43:26 +0530

    Updated httpd pack

---
+++ 12.Httpd_Pack/httpd/actions/stop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+OSID=$(cat /etc/os-release | grep -w "ID" | cut -d "=" -f 2)
+OSID=${OSID%\"}
+OSID=${OSID#\"}
+if [[ "${OSID}" == "ubuntu" ]]
+then
+  echo "The os is identified as Ubuntu"
+  systemctl stop apache2 
+elif [[ "${OSID}" == "rhel" ]]
+then
+  echo "The os is identified as RHEL"
+  systemctl stop httpd 
+else
+  echo "The os is unkown"
+  exit 5
+fi
+
